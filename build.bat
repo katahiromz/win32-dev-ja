d1a2e8acc501e2d5730cbe6a7fa9e2a32b0fb9fe
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2020-08-23 13:14:04 +0900

    add some files for epub

---
+++ build.bat
@@ -0,0 +1,1 @@
+pandoc -t epub3 -o win32-dev-ja.epub title.txt README.md --epub-cover-image=cover.png --css=github.css --epub-metadata=metadata.xml
--- build.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-pandoc -t epub3 -o test.epub title.txt README.md --epub-cover-image=cover.png --epub-stylesheet=github.css --epub-metadata=metadata.xml
