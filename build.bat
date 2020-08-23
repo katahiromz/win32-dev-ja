c994bfdd1b0877e5183d9247e896092a0627432a
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2020-08-23 20:57:04 +0900

    update

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-pandoc -t epub3 -o win32-dev-ja.epub title.txt README.md --epub-cover-image=cover.png --css=github.css --epub-metadata=metadata.xml
+pandoc -t epub -o win32-dev-ja.epub title.txt README.md --epub-cover-image=cover.png --css=github.css --epub-metadata=metadata.xml
